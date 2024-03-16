a9bad4a4cdb6b1105afcf9ff83b4809f2ed37352
lego <oleg.lukasonok@lt.ibm.com>
2024-03-17 00:42:47 +0200

    ...

--- .scripts/nvm/lib/-package-install.sh
+++ .scripts/nvm/lib/-package-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 # -------------------------------------------------------------------------------------
 #
 # PLACEHOLDER_COPYRIGHT
@@ -9,11 +9,17 @@
 
 . ./.scripts/loggers/lib/--index-api.sh
 
-. ./.scripts/os-manager/lib/--index.sh
+. ./.scripts/os-manager/lib/-package-install.sh
+
+. ./.scripts/zshrc/lib/-add-line-if-not-exists.sh
 
 _nvm_package_install() {
     local FUNCTION_NAME="_nvm_package_install"
     _loggers_info "${FUNCTION_NAME}"
 
     _osManager_packageInstall "nvm"
+
+    _zshrc_addLineIfNotExists '# [NVM] BELOW 3 LINES REQUIRED'
+    _zshrc_addLineIfNotExists 'export NVM_DIR="$HOME/.nvm"'
+    _zshrc_addLineIfNotExists '. $(brew --prefix nvm)/nvm.sh'
 }
--- .scripts/nvm/lib/-package-uninstall.sh
+++ .scripts/nvm/lib/-package-uninstall.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 # -------------------------------------------------------------------------------------
 #
 # PLACEHOLDER_COPYRIGHT
@@ -9,11 +9,17 @@
 
 . ./.scripts/loggers/lib/--index-api.sh
 
-. ./.scripts/os-manager/lib/--index.sh
+. ./.scripts/os-manager/lib/-package-uninstall.sh
+
+. ./.scripts/zshrc/lib/-delete-line-if-exists.sh
 
 _nvm_package_uninstall() {
     local FUNCTION_NAME="_nvm_package_uninstall"
     _loggers_info "${FUNCTION_NAME}"
 
     _osManager_packageUninstall "nvm"
+
+    _zshrc_deleteLineIfExists '# [NVM] BELOW 3 LINES REQUIRED'
+    _zshrc_deleteLineIfExists 'export NVM_DIR="$HOME/.nvm"'
+    _zshrc_deleteLineIfExists '. $(brew --prefix nvm)/nvm.sh'
 }
