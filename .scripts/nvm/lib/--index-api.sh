ea35f377feb49b6a491a73386891fa574e28a805
lego <oleg.lukasonok@lt.ibm.com>
2024-03-16 23:41:03 +0200

    ...

---
+++ .scripts/nvm/lib/--index-api.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# -------------------------------------------------------------------------------------
+#
+# PLACEHOLDER_COPYRIGHT
+#
+# PLACEHOLDER_LICENCE
+#
+# -------------------------------------------------------------------------------------
+
+. ./.scripts/nvm/lib/-package-info.sh
+. ./.scripts/nvm/lib/-package-install.sh
+. ./.scripts/nvm/lib/-package-uninstall.sh
---
+++ .scripts/nvm/lib/--index.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# -------------------------------------------------------------------------------------
+#
+# PLACEHOLDER_COPYRIGHT
+#
+# PLACEHOLDER_LICENCE
+#
+# -------------------------------------------------------------------------------------
+#
+#       --> passed parameters are read & exported environment variables
+#
+. ./.scripts/nvm/lib/--env-vars-reader.sh
+#
+#       --> required environment variables are validated for existance
+#
+. ./.scripts/nvm/lib/--env-vars-validator.sh
+#
+#       --> available functions are imported/exported
+#
+. ./.scripts/nvm/lib/--index-api.sh
+# -------------------------------------------------------------------------------------
